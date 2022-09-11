c8ee5692c82f30a597266e8f99c8be8c9875bc85
Nick <chidojiro@gmail.com>
2022-09-11 23:02:48 +0700

    feat: common icons

---
+++ packages/common/icons/index.ts
@@ -0,0 +1,11 @@
+export {
+  FaHome as HomeIcon,
+  FaSignInAlt as SignInIcon,
+  FaSignOutAlt as SignOutIcon,
+  FaPlus as PlusIcon,
+  FaTimes as TimesIcon,
+  FaCoffee as CoffeeIcon,
+  FaFlag as FlagIcon,
+  FaThumbsUp as ThumbsUpIcon,
+  FaThumbsDown as ThumbsDownIcon,
+} from 'react-icons/fa';
