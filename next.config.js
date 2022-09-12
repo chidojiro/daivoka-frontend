495bda16e8cfb3fd326c37841dd8bf2d315fc81d
Nick <chidojiro@gmail.com>
2022-09-12 18:34:00 +0700

    feat: reconfig alias

--- next.config.js
+++ next.config.js
@@ -1,7 +1,46 @@
-/** @type {import('next').NextConfig} */
+const path = require('path');
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+/**
+ * @type {import('next/dist/next-server/server/config').NextConfig}
+ **/
+
+const withTM = require('next-transpile-modules')(['hsk-headless']); // pass the modules you would like to see transpiled
+
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-}
+  webpack(config) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, 'packages'),
+    };
+    config.module.rules.push({
+      test: /\.svg$/,
+      issuer: { and: [/\.(js|ts)x?$/] },
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: {
+            svgoConfig: {
+              plugins: [
+                {
+                  name: 'prefixIds',
+                  active: true,
+                },
+                {
+                  name: 'removeViewBox',
+                  active: false,
+                },
+              ],
+            },
+          },
+        },
+      ],
+    });
 
-module.exports = nextConfig
+    return config;
+  },
+};
+module.exports = withBundleAnalyzer(withTM(nextConfig), { silent: true });
